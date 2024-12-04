60633f1a2695ce9da03810a4d45c1a225993a37f
jiyoon <rlawldbs9128@gmail.com>
2024-12-05 01:39:47 +0900

    [update] 주문, 주문조회, 주문상태변경 구현

--- src/main/java/com/example/whattheeat/controller/OrderController.java
+++ src/main/java/com/example/whattheeat/controller/OrderController.java
@@ -1,4 +1,52 @@
 package com.example.whattheeat.controller;
 
+import com.example.whattheeat.dto.OrderRequestDto;
+import com.example.whattheeat.dto.OrderResponseDto;
+import com.example.whattheeat.service.OrderService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    //주문 - 고객
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@RequestBody OrderRequestDto requestDto,
+                                                        HttpSession session){
+        Long authenticatedCustomerId = (Long) session.getAttribute("authenticatedUserId");
+        String role = (String) session.getAttribute("authenticatedUserRole");
+
+        if(authenticatedCustomerId == null || !"CUSTOMER".equals(role)){
+            throw new IllegalArgumentException("로그인한 고객만 주문을 할 수 있습니다.");
+        }
+
+        OrderResponseDto responseDto = orderService.createOrder(requestDto, authenticatedCustomerId);
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+    //주문 상태 변경 - 사장님
+    @PutMapping("/{orderId}/status")
+    public ResponseEntity<OrderResponseDto> updateOrderStatus(
+            @PathVariable Long orderId,
+            @RequestBody OrderRequestDto requestDto,
+            HttpSession session
+    ){
+        Long authenticatedOwnerId = (Long) session.getAttribute("authenticatedUserId");
+        String role = (String) session.getAttribute("authenticatedUserRole");
+
+        if(authenticatedOwnerId == null || !"OWNER".equals(role)){
+            throw new IllegalArgumentException("사장님만 주문 상태를 변경할 수 있습니다.");
+        }
+
+        OrderResponseDto responseDto = orderService.updateOrderStatus(orderId,authenticatedOwnerId, requestDto.getOrderStatus());
+        return ResponseEntity.ok(responseDto);
+    }
+
 }
--- src/main/java/com/example/whattheeat/service/OrderService.java
+++ src/main/java/com/example/whattheeat/service/OrderService.java
@@ -1,4 +1,102 @@
 package com.example.whattheeat.service;
 
+import com.example.whattheeat.dto.OrderRequestDto;
+import com.example.whattheeat.dto.OrderResponseDto;
+import com.example.whattheeat.entity.Order;
+import com.example.whattheeat.enums.OrderStatus;
+import com.example.whattheeat.repository.OrderRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MenuRepository menuRepository;
+    private final ShopRepository shopRepository;
+
+    //주문하기 - 고객 권한
+    @Transactional
+    public OrderResponseDto createOrder(OrderRequestDto requestDto, Long authenticatedCustomerId){
+
+        Menu menu = menuRepository.findById(requestDto.getMenuId())
+                .orElseThrow(() -> new IllegalArgumentException("없는 메뉴입니다."));
+
+        Shop shop = shopRepository.findById(requestDto.getShopId())
+                .orElseThrow(() -> new IllegalArgumentException("가게를 찾을 수 없습니다."));
+
+
+        Order order = Order.builder()
+                .customer(authenticatedCustomerId)
+                .menu(menu)
+                .shop(shop)
+                .quantity(requestDto.getQuantity())
+                .address(requestDto.getAddress())
+                .orderStatus(OrderStatus.ORDERED)
+                .build();
+
+        Order savedOrder = orderRepository.save(order);
+        return convertToResponseDto(savedOrder);
+    }
+
+    //주문 상태 변경 - 사장님
+    @Transactional
+    public OrderResponseDto updateOrderStatus(Long orderId, Long authenticatedOwnerId, OrderStatus newStatus){
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalArgumentException("주문을 찾을 수 없습니다."));
+
+        if(!order.getShop().getOwner().getId().equals(authenticatedOwnerId)){
+            throw new IllegalArgumentException("해당 주문을 변경할 권한이 없습니다.");
+        }
+
+        order.setOrderStatus(newStatus);
+
+        return convertToResponseDto(order);
+    }
+
+    //주문내역 조회 - 고객
+    @Transactional(readOnly = true)
+    public List<OrderResponseDto> getOrderByCustomer(Long authenticatedCustomerId){
+        List<Order> orders = orderRepository.findByCustomerId(authenticatedCustomerId);
+
+        return orders.stream()
+                .map(this::convertToResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    //주문내역 조회 - 가게
+    @Transactional(readOnly = true)
+    public List<OrderResponseDto> getOrderByShop(Long shopId, Long authenticatedOwnerId){
+        Shop shop = shopRepository.findById(shopId)
+                .orElseThrow(() -> new IllegalArgumentException("가게를 찾을 수 없습니다."));
+
+        if(!shop.getOwner().getId().equals(authenticatedOwnerId)){
+            throw new IllegalArgumentException("해당 가게의 주문을 조회할 권한이 없습니다.");
+        }
+
+        List<Order> orders = orderRepository.findByShopId(shopId);
+
+        return orders.stream()
+                .map(this::convertToResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    private OrderResponseDto convertToResponseDto(Order order){
+        return new OrderResponseDto(
+                order.getId(),
+                order.getCustomer().getId(),
+                order.getMenu().getId(),
+                order.getShop().getId(),
+                order.getQuantity(),
+                order.getTotalPrice(),
+                order.getAddress(),
+                order.getOrderStatus()
+        );
+    }
 }
