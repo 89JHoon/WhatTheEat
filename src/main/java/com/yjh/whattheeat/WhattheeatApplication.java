b92b9dcb7ead2893d7b8f1d01248fd6208456110
cooleHoonst <88084780+89JHoon@users.noreply.github.com>
2024-12-04 13:26:55 +0900

    first commit

---
+++ src/main/java/com/yjh/whattheeat/WhattheeatApplication.java
@@ -0,0 +1,13 @@
+package com.yjh.whattheeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattheeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhattheeatApplication.class, args);
+    }
+
+}
