a06d36dd3234f201c648863ece17a6562dc6aa8f
cooleHoonst <88084780+89JHoon@users.noreply.github.com>
2024-12-06 02:18:57 +0900

    [move] GlobalExceptionHandler 통합

--- src/main/java/com/example/whattheeat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/whattheeat/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.whattheeat.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,4 +22,23 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> responseStatusExcep(ResponseStatusException e) {
         return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
     }
+
+    //Shop 관련
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation (DataIntegrityViolationException e){
+        log.error("가게 이름 중복 오류. :{}",e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body("이미 등록된 가게입니다.");
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+
 }
