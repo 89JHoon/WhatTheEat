e2173f09e050ed1888f937c395a99e8940138f61
jiyoon <rlawldbs9128@gmail.com>
2024-12-05 12:53:42 +0900

    [refactor] class -> interface

--- src/main/java/com/example/whattheeat/repository/OrderRepository.java
+++ src/main/java/com/example/whattheeat/repository/OrderRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public class OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomerId(Long customerId);
 
     List<Order> findByShopId(Long shopId);
