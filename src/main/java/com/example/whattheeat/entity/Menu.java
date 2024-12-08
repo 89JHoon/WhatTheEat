daeab301a28e243066ffd4b0757b5bc8c41a6ab8
fbqudrlf09 <fbqudrlf09@naver.com>
2024-12-08 10:26:33 +0900

    [bugfix] 메뉴 삭제 관련 sql 오류 수정

--- src/main/java/com/example/whattheeat/entity/Menu.java
+++ src/main/java/com/example/whattheeat/entity/Menu.java
@@ -2,19 +2,15 @@ package com.example.whattheeat.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-
-import java.math.BigDecimal;
-
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 
-@SQLDelete(sql = "update menu set deleted_at = current_timestamp where id = ?")
-@SQLRestriction("isDeleted = true")
+@SQLDelete(sql = "update menu set is_deleted = true, deleted_at = current_timestamp where id = ?")
+@SQLRestriction("is_deleted = false")
 @Entity
 @Getter
 @Table(name = "menu")
